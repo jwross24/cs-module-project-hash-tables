45a65d93271c3fa03d37bc27035e601a4cede740
jwross24 <jwross24@users.noreply.github.com>
2020-06-04 06:18:34 -0400

    finish lookup table task

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,6 @@
-# Your code here
+import math
+import random
+from time import sleep
 
 
 def slowfun_too_slow(x, y):
@@ -9,18 +11,25 @@ def slowfun_too_slow(x, y):
 
     return v
 
-def slowfun(x, y):
+
+mod_result = {}
+
+
+def slowfun(x, y, mod_result):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    if (x, y) not in mod_result:
+        mod_result[(x, y)] = slowfun_too_slow(x, y)
+        print(f"Added {len(mod_result)}/36 results so far")
+        sleep(1)
 
+    return mod_result[(x, y)]
 
 
 # Do not modify below this line!
-
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f'{i}: {x},{y}: {slowfun(x, y, mod_result)}')
